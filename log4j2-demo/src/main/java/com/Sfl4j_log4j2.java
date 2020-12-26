3e47bee5b1a53dc41055a93fc21b1896b50c3177
Fleur520 <392373637@qq.com>
2020-12-26 17:24:16 +0800

    Add files via upload

---
+++ log4j2-demo/src/main/java/com/Sfl4j_log4j2.java
@@ -0,0 +1,41 @@
+package com;
+
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Calendar;
+
+public class Sfl4j_log4j2 {
+
+    /**
+     * 定义日志器对象
+     */
+    private static Logger log = LoggerFactory.getLogger(Sfl4j_log4j2.class);
+
+
+
+    public static void main(String[] args) {
+
+        long start = Calendar.getInstance().getTime().getTime();
+        log.warn(String.valueOf(start));
+
+        for (int i = 0; i < 100000; i++) {
+            log.trace("======trace"+i);
+            log.debug("======debug"+i);
+            log.info("======info"+i);
+            log.warn("======warn"+i);
+            log.error("======error"+i);
+        }
+        long end =Calendar.getInstance().getTime().getTime();
+
+        log.warn(String.valueOf(end));
+
+        log.warn("fininsh"+String.valueOf(end -start));
+        System.out.println("fininshabc"+String.valueOf(end -start));
+
+    }
+
+}
